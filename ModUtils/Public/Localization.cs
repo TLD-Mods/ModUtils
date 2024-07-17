c6e7fd5504fccf3469c79bdb3d780e88f0deb8e7
Deadman <47807486+Deaadman@users.noreply.github.com>
2024-07-17 16:36:35 +0930

    new method to register localizations

---
+++ ModUtils/Public/Localization.cs
@@ -0,0 +1,28 @@
+﻿using LocalizationUtilities;
+
+namespace ModUtils;
+
+/// <summary>
+/// Utility methods for anything regarding LocalizationUtilities
+/// </summary>
+public static class Localization
+{
+    /// <summary>
+    /// Register custom localizations within The Long Dark.
+    /// </summary>
+    /// <param name="jsonFilePath">An embedded path to your localization .json file. E.g. ItemRarities.Resources.Localization.json</param>
+    public static void RegisterLocalizationKeys(string jsonFilePath)
+    {
+        if (string.IsNullOrWhiteSpace(jsonFilePath)) throw new ArgumentNullException(nameof(jsonFilePath));
+
+        using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(jsonFilePath);
+        if (stream == null) throw new FileNotFoundException($"Resource not found: {jsonFilePath}");
+
+        using var reader = new StreamReader(stream);
+        var jsonText = reader.ReadToEnd();
+
+        if (string.IsNullOrWhiteSpace(jsonText)) throw new InvalidDataException("JSON content is empty or whitespace.");
+
+        LocalizationManager.LoadJsonLocalization(jsonText);
+    }
+}
